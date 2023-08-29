cc7e636f92188e0a6abf4ae64c48bce6e990824b
Eliane Coelho <ellianee@hotmail.com>
2023-08-28 23:22:24 -0300

    criado projeto conta Banco em java básico

---
+++ JAVA BÁSICO/CONTA-BANCO/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+     Scanner sc = new Scanner(System.in);
+
+       int conta = 1021;
+       String agencia = "067-8";
+       String nomeCliente = "Mario Andrade";
+       Double saldo = 237.48;
+
+     System.out.println("Insira Agência"); 
+     agencia = sc.nextLine(); 
+        
+     System.out.println("Isira Conta"); 
+     conta = sc.nextInt(); 
+    
+     System.out.println("Nome do Cliente"); 
+     nomeCliente = sc.nextLine();     
+
+      System.out.println("Olá " + nomeCliente + ". Obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + " conta "+ conta+ ", e seu saldo " + saldo + " já está disponível para saque.");
+       
+    }
+}
